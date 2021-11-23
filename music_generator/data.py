aa72fe5e7176d6485f27ae5e52b78f4e3d9f036a
Matheus Oliveira <matheus_so@yahoo.com.br>
2021-11-23 13:58:50 -0300

    fixed multitrack_to_midi function to correctly generate midi files for each file and added to possibility to change the path in order to have better usability in other folders

--- music_generator/data.py
+++ music_generator/data.py
@@ -1,23 +1,24 @@
 import pypianoroll
 import glob
 
-def get_npz_data(n):
+
+def get_npz_data(n, data_path='raw_data/lpd_5/lpd_5_cleansed'):
     ''''
     Load n number of npz files from all of the folders contained in the raw_data folder
     '''
     # get a list of the possible filepaths for the npz files
-    files = glob.glob('raw_data/lpd_5/lpd_5_cleansed/**/**/**/**/*.npz')
+    files = glob.glob(f'{data_path}/**/**/**/**/*.npz')
 
     # Load n files from the dataset
     multitracks = [pypianoroll.load(fp) for fp in files[0:n]]
     return multitracks
 
 
-def multitrack_to_midi(multitracks):
+def multitrack_to_midi(multitracks, save_path='raw_data/midi'):
     '''
     Given a list of multitrack objects convert each of them into a midi file
     and save it on the raw_data folder
     '''
     for i in range(len(multitracks)):
-        pypianoroll.write(f'raw_data/midi/track{i}.midi', multitracks[1])
+        pypianoroll.write(f'{save_path}/track{i}.midi', multitracks[i])
     return 'midi files saved successfully'
