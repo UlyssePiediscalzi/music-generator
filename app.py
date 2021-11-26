28595eade8e2aedae2020dff26873c224b6d56ab
William Calderon <wecalderonc@unal.edu.co>
2021-11-26 12:11:03 -0300

    change data

--- app.py
+++ app.py
@@ -1,45 +1,11 @@
-import ipdb
-from tensorflow.python.lib.io import file_io
-import tensorflow as tf
-from io import BytesIO
-import pypianoroll
 import numpy as np
 from music_generator.data import get_npz_data
-
-### GCP configuration - - - - - - - - - - - - - - - - - - -
-
-### GCP Project - - - - - - - - - - - - - - - - - - - - - -
-
-### GCP Storage - - - - - - - - - - - - - - - - - - - - - -
-
-BUCKET_NAME = 'music-generator-713'
-
-##### Data  - - - - - - - - - - - - - - - - - - - - - - - -
-
-BUCKET_TRAIN_DATA_PATH = 'data/'
-
-##### Training  - - - - - - - - - - - - - - - - - - - - - -
-
-##### Model - - - - - - - - - - - - - - - - - - - - - - - -
-
-# model folder name (will contain the folders for all trained model versions)
-#MODEL_NAME = 'taxifare'
-
-#multi = pypianoroll.load("gs://music-generator-713/data/lpd_5/lpd_5_cleansed/R/R/A/TRRRAJP128E0793859/2c78d25cb52eb48f767186ddaf11b191.npz")
-#     multitrack = pypianoroll.load(f)
-
-#print(multi)
-
-# client = storage.Client()
-# bucket = client.bucket(BUCKET_NAME)
-
-# blobs = list(bucket.list_blobs(prefix='data/'))
-
-# read_output = blobs[10].download_as_string()
-# pypianoroll.load(blobs[10])
-
-# print(read_output)
+import streamlit as st
 
 
+st.markdown("""# This is a header
+## This is a sub header
+This is text""")
 
-ipdb.set_trace()
+
+st.write(get_npz_data(10, "gcp"))
