ea070f371b8dab5e48750c6daa293e7e79b8bbc6
Leo Kewitz <leo@opencollective.com>
2021-11-26 14:50:38 -0300

    feat: add note parsing functions for mid files

--- music_generator/midi.py
+++ music_generator/midi.py
@@ -2,16 +2,21 @@ import pypianoroll
 import numpy as np
 import matplotlib.pyplot as plt
 import IPython
+from music21 import converter, instrument, note, chord
+
 from music_generator.data import get_npz_data
+
 
 def flatten(pianorolls):
     """Automatically joins multiple pianorolls into a single sequence."""
 
-    rolls, steps, pitch  = pianorolls.shape
+    rolls, steps, pitch = pianorolls.shape
     return pianorolls.reshape(rolls*steps, pitch)
+
 
 def get_instrument(mtrack, name):
     return [track for track in mtrack.tracks if track.name == name and type(track) is pypianoroll.track.StandardTrack][0]
+
 
 def plot_pianoroll(pianoroll):
     """Plots pianoroll pitches through time for given pianoroll.
@@ -27,15 +32,18 @@ def plot_pianoroll(pianoroll):
 
     if len(pianoroll.shape) == 3:
         pianoroll = flatten(pianoroll)
-    fig, ax = plt.subplots(figsize=(14,4))
-    return pypianoroll.plot_pianoroll(ax=ax, pianoroll=pianoroll);
+    fig, ax = plt.subplots(figsize=(14, 4))
+    return pypianoroll.plot_pianoroll(ax=ax, pianoroll=pianoroll)
+
 
 def create_multitrack(basemultitrack, pianoroll):
     if len(pianoroll.shape) == 3:
         pianoroll = flatten(pianoroll)
-    track = pypianoroll.StandardTrack(program=0, is_drum=False, pianoroll=pianoroll)
+    track = pypianoroll.StandardTrack(
+        program=0, is_drum=False, pianoroll=pianoroll)
     tempo = basemultitrack.tempo[:pianoroll.shape[0]]
     return pypianoroll.Multitrack(tempo=tempo, tracks=[track])
+
 
 def play_pianoroll(multitrack, pianoroll):
     """Returns an IPython player for given pianoroll.
@@ -52,6 +60,7 @@ def play_pianoroll(multitrack, pianoroll):
     mt = create_multitrack(multitrack, pianoroll)
     return IPython.display.Audio(data=mt.to_pretty_midi().synthesize(), rate=44100)
 
+
 def sample_multitrack(multitrack, track, x_qnotes=4, y_qnotes=2):
     pianoroll = track.pianoroll
     qnote = multitrack.resolution
@@ -59,28 +68,30 @@ def sample_multitrack(multitrack, track, x_qnotes=4, y_qnotes=2):
     X = []
     y = []
     for i in range(int(np.floor(quarter_notes/(x_qnotes+y_qnotes)))):
-        X_start = i*x_qnotes *qnote
-        X_end = ((i+1)*x_qnotes) *qnote
+        X_start = i*x_qnotes * qnote
+        X_end = ((i+1)*x_qnotes) * qnote
         y_start = X_end
-        y_end = ((i+1)*x_qnotes + y_qnotes) *qnote
+        y_end = ((i+1)*x_qnotes + y_qnotes) * qnote
         X.append(pianoroll[X_start:X_end])
         y.append(pianoroll[y_start:y_end])
     return np.array(X), np.array(y)
 
+
 def sample_roll(resolution, roll, x_qnotes=4, y_qnotes=2):
     pianoroll = roll
-    qnote = resolution 
+    qnote = resolution
     quarter_notes = int(pianoroll.shape[0]/qnote)
     X = []
     y = []
     for i in range(int(np.floor(quarter_notes/(x_qnotes+y_qnotes)))):
-        X_start = i*x_qnotes *qnote
-        X_end = ((i+1)*x_qnotes) *qnote
+        X_start = i*x_qnotes * qnote
+        X_end = ((i+1)*x_qnotes) * qnote
         y_start = X_end
-        y_end = ((i+1)*x_qnotes + y_qnotes) *qnote
+        y_end = ((i+1)*x_qnotes + y_qnotes) * qnote
         X.append(pianoroll[X_start:X_end])
         y.append(pianoroll[y_start:y_end])
     return np.array(X), np.array(y)
+
 
 def multi_sample_pianorolls(pianorolls, qnote=4, x_qnotes=4, y_qnotes=4):
     '''pianorolls = output of 'fetching_instrument_pianorolls' function in data.py.
@@ -129,3 +140,19 @@ def multi_track_train_test(n_tracks, n_steps):
     y_test = y_test.reshape(y_test.shape[0] * y_test.shape[1], 128)[:n_steps]
     return np.array(X_final), np.array(y_final), np.array(X_test), np.array(
         y_test), multitracks
+
+
+def parseNote(element):
+    if isinstance(element, note.Note):
+        return f"{element.pitch} for {element.duration.quarterLength}"
+    elif isinstance(element, chord.Chord):
+        c = '.'.join(f"{n}" for n in element.normalOrder)
+        return f"{c} for {element.duration.quarterLength}"
+
+
+def get_notes(midi):
+    instruments = instrument.partitionByInstrument(midi)
+    piano = [i for i in instruments if i.id == 'Piano'][0]
+    notes = [parseNote(n) for n in piano.recurse() if type(n)
+             is note.Note or type(n) is chord.Chord]
+    return notes
