85cd4b1471ba6e555a9224f9cf8a035e964ed743
William Calderon <wecalderonc@unal.edu.co>
2021-11-29 10:29:00 -0300

    playing with api and midi

--- music_generator/build_music.py
+++ music_generator/build_music.py
@@ -1,7 +1,32 @@
 
+import h5py
+import tensorflow as tf
+import joblib
+from flask import send_file
+
 def create_song():
+  ###### REAL BEHAVIOR #######
   #bring the model
-  #model.predict(song)
+  #download_model.predict(song)
   #the song maybe from the user or directly saved in your gcp
   #save the song in new_song
-  return new_song
+  ################
+  ###### DUMMY BEHAVIOR #######
+  #load a song
+  new_song = open('midi_songs_Eternal_Harvest.mid', 'rb')
+  return send_file(new_song, mimetype='audio/midi')
+
+
+def download_model():
+  MODEL_NAME = "music-generator-model"
+  BUCKET_NAME = "music-generator-713"
+  MODEL_VERSION = "0.1"
+  LOCAL_MODEL_NAME = "model.h5"
+  BUCKET_NAME = 'music-generator-713'
+  BUCKET_TRAIN_DATA_PATH = 'data/'
+
+  gcs_path = f"gs://{BUCKET_NAME}/models/{MODEL_NAME}/{MODEL_VERSION}/{LOCAL_MODEL_NAME}"
+  print(gcs_path)
+  loaded_model = tf.io.gfile.GFile(gcs_path, 'rb')
+  model_gcs = h5py.File(loaded_model, 'r')
+  print(model_gcs)
