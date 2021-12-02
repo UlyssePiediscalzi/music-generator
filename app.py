f8bc69dafd2a1c39891da43a83b2a22533de8de2
William Calderon <wecalderonc@unal.edu.co>
2021-12-02 17:49:00 -0300

    adding three notes

--- app.py
+++ app.py
@@ -11,81 +11,112 @@ from scipy.io import wavfile
 import numpy as np
 import io
 
-st.set_page_config(
-    page_title="Music-Generator 1.0",  # => Quick reference - Streamlit
-    page_icon="🐍",
-    layout="centered",  # wide
-    initial_sidebar_state="auto")  # collapsed
-
-st.title('Music-Generator 1.0: Music Generation with an LSTM Neural Network')
-
-###############################
-
 def get_binary_file_downloader_html(bin_file, file_label='File'):
-    #ipdb.set_trace()
-
     with open(bin_file, 'rb') as f:
         data = f.read()
     bin_str = b64encode(data).decode()
     href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{path.basename(bin_file)}">Download {file_label}</a>'
     return href
-#################################
 
-col1, col2, col3 = st.columns(3)
-all_notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+st.set_page_config(
+    page_title="Piano Melodies Generator 1.0",  # => Quick reference - Streamlit
+    page_icon="🐍",
+    layout="centered",  # wide
+    initial_sidebar_state="auto")  # collapsed
 
-with col1:
-  st.subheader("Select 1st Note")
-  first_note = st.selectbox('Select first note', all_notes)
+st.title('Piano Melodies Generator 1.0')
 
-with col2:
-  st.subheader("Select 2nd Note")
-  second_note = st.selectbox('Select second note', all_notes)
+# if st.checkbox("Run", key="A"):
+if st.button("Generate new notes"):
+    url = "https://music-generator-api-zyjtckkcoa-uc.a.run.app/random_notes"
+    local_url = "http://localhost:8000/random_notes"
 
-with col3:
-  st.subheader("Select 3rd Note")
-  third_note = st.selectbox('Select third note', all_notes)
+    with st.spinner('Wait for it...'):
+      response = requests.get(local_url)
+      response = response.json()
+      st.success('Notes created!')
 
-notes = [note.Note(first_note), note.Note(second_note), note.Note(third_note)]
-
-st.header("Notes:")
-st.subheader(f"{notes}  .")
-
-if st.button('CREATE NEW SONG'):
-    url = "https://music-generator-api-zyjtckkcoa-uc.a.run.app/predict"
-    local_url = "http://localhost:8000/predict"
-    #CALL API
-    response = requests.get(url)
-    response = response.json()
-    Music_notes = response['notes']
-    Music_notes = json.loads(Music_notes)
-
-    Music_durations = response['durations']
-    Music_durations = json.loads(Music_durations)
-
-    #FINISHING CALLING API
-
-    # CREATE MELODY
-    Melody = build_music.chords_n_notes(Music_notes, Music_durations, notes)
-    Melody_midi = stream.Stream(Melody)
-    song_name = f"test_song_number_{randrange(1000)}"
-    fpath = f"{song_name}.mid"
-    midi_file = Melody_midi.write('midi', fp=fpath)
-    st.markdown(get_binary_file_downloader_html(fpath, 'MIDI'), unsafe_allow_html=True)
+    st.session_state.user_input_notes = json.loads(response["user_input_notes"])
+    st.session_state.user_input_durations = json.loads(response["user_input_durations"])
+    basic_melody = build_music.chords_n_notes(st.session_state.user_input_notes, st.session_state.user_input_durations)
+    basic_melody_midi = stream.Stream(basic_melody)
+    three_notes_sounds = f"random_notes_{randrange(1000)}"
+    fpath = f"{three_notes_sounds}.mid"
+    midi_file = basic_melody_midi.write('midi', fp=fpath)
 
     with st.spinner(f"Transcribing to FluidSynth"):
       midi_data = pretty_midi.PrettyMIDI(midi_file)
       audio_data = midi_data.synthesize()
       audio_data = np.int16(
-          audio_data / np.max(np.abs(audio_data)) * 32767 * 0.9
+        audio_data / np.max(np.abs(audio_data)) * 32767 * 0.9
       )  # -- Normalize for 16 bit audio https://github.com/jkanner/streamlit-audio/blob/main/helper.py
 
-      virtualfile = io.BytesIO()
-      wavfile.write(virtualfile, 44100, audio_data)
+      virtualfile_basic = io.BytesIO()
+      wavfile.write(virtualfile_basic, 44100, audio_data)
 
+    col1, col2, col3 = st.columns(3)
+    col1.metric("First Note", st.session_state.user_input_notes[0])
+    col2.metric("Second Note", st.session_state.user_input_notes[1])
+    col3.metric("Third Note", st.session_state.user_input_notes[2])
+
+    st.audio(virtualfile_basic)
+
+
+# if st.checkbox("Run", key="B"):
+def update_first():
+    st.session_state.second = st.session_state.first
+
+if st.button('Create new song'):
+    url = "https://music-generator-api-zyjtckkcoa-uc.a.run.app/predict"
+    local_url = "http://localhost:8000/predict"
+
+
+    with st.spinner('Wait for it...'):
+
+      params = {
+          "user_input_notes": json.dumps(st.session_state.user_input_notes),
+          "user_input_durations": json.dumps(st.session_state.user_input_durations)
+      }
+      response = requests.get(local_url, params = params)
+      response = response.json()
+      Music_notes = response['notes']
+      Music_notes = json.loads(Music_notes)
+
+      Music_durations = response['durations']
+      Music_durations = json.loads(Music_durations)
+
+      col1, col2, col3 = st.columns(3)
+      col1.metric("First Note", st.session_state.user_input_notes[0])
+      col2.metric("Second Note", st.session_state.user_input_notes[1])
+      col3.metric("Third Note", st.session_state.user_input_notes[2])
+
+      # CREATE MELODY
+      Melody = build_music.chords_n_notes(Music_notes, Music_durations)
+      # build_music.show(Melody)
+      Melody_midi = stream.Stream(Melody)
+      song_name = f"test_song_number_{randrange(1000)}"
+      fpath = f"{song_name}.mid"
+      midi_file = Melody_midi.write('midi', fp=fpath)
+
+      with st.spinner(f"Transcribing to FluidSynth"):
+        midi_data = pretty_midi.PrettyMIDI(midi_file)
+        audio_data = midi_data.synthesize()
+        audio_data = np.int16(
+            audio_data / np.max(np.abs(audio_data)) * 32767 * 0.9
+        )  # -- Normalize for 16 bit audio https://github.com/jkanner/streamlit-audio/blob/main/helper.py
+
+        virtualfile = io.BytesIO()
+        wavfile.write(virtualfile, 44100, audio_data)
+
+        st.success('Listen the next hit!')
     st.audio(virtualfile)
-
-else:
-    st.write('I was not clicked 😞')
+    col7, col8, col9 = st.columns(3)
+    #col2.markdown(get_binary_file_downloader_html(fpath, 'MIDI'), unsafe_allow_html=True)
+    st.download_button(
+        label="Download midi file",
+        data=fpath,
+        file_name=fpath,
+        mime='audio/mid',
+    )
 
 
--- music_generator/build_music.py
+++ music_generator/build_music.py
@@ -1,10 +1,9 @@
 import numpy as np
-from music21 import note, chord, stream, duration
+import music21
+from music21 import *
 
-def chords_n_notes(SnippetNote, SnippetDur, notes):
-
-
-    Melody = notes
+def chords_n_notes(SnippetNote, SnippetDur):
+    Melody = []
     offset = 0  # Incremental
     for i, k in zip(SnippetNote, SnippetDur):
       #If it is chord
@@ -38,27 +37,5 @@ def chords_n_notes(SnippetNote, SnippetDur, notes):
     return Melody_midi
 
 
-# def chords_n_notes(Snippet):
-#   Melody = []
-#   offset = 0  # Incremental
-#   for i in Snippet:
-#       #If it is chord
-#       if ("." in i or i.isdigit()):
-#           chord_notes = i.split(".")  # Seperating the notes in chord
-#           notes = []
-#           for j in chord_notes:
-#               inst_note = int(j)
-#               note_snip = note.Note(inst_note)
-#               notes.append(note_snip)
-#               chord_snip = chord.Chord(notes)
-#               chord_snip.offset = offset
-#               Melody.append(chord_snip)
-#       # pattern is a note
-#       else:
-#           note_snip = note.Note(i)
-#           note_snip.offset = offset
-#           Melody.append(note_snip)
-#       # increase offset each iteration so that notes do not stack
-#       offset += 0.5
-#   Melody_midi = stream.Stream(Melody)
-#   return Melody_midi
+
+
